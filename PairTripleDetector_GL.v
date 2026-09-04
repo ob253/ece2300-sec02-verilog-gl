b0b16cd0c72efee345ad5c00fbba21eae0a235c9
ob253 <ob253@cornell.edu>
2026-09-04 16:29:27 -0400

    done

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -14,18 +14,13 @@ module PairTripleDetector_GL
   input wire  in2,
   output wire out
 );
-
-  //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // Implement pair/triple detector using explicit gate-level modeling
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // You will need to remove the ECE2300_UNUSED and ECE2300_FLOATING
-  // macros and then replace them with your implementation (this is also
-  // true for all lab assignments).
-
-  `ECE2300_UNUSED( in0 );
-  `ECE2300_UNUSED( in1 );
-  `ECE2300_UNUSED( in2 );
-  `ECE2300_FLOATING( out );
+wire w;
+or( w, in0, in1 );
+wire y;
+and( y, w, in2 );
+wire u;
+and( u, in0, in1 );
+or( out, y, u );
 
 endmodule
 
